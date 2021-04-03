1ff261e022de3bd55e8dc7535b46b8e629c78735
Røkshan <rkmarechal@gmail.com>
2021-04-03 16:16:44 +0200

    added simple test class to implement

---
+++ code/server/src/test/java/minihearthstone/entities/BasicCardTest.java
@@ -0,0 +1,14 @@
+package minihearthstone.entities;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class BasicCardTest {
+
+	@Test
+	public void testCopy() {
+		/// TODO : implement test
+		assertTrue(true);
+	}
+}
